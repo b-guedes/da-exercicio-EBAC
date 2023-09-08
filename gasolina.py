231f284ceaefb5e99fdc1105bdef2a7044880de3
b-guedes <xguedesx@hotmail.com>
2023-09-08 19:44:30 +0000

    arquivos criados <gasolina.csv>, <gasolina.py>, <gasolina.png>

---
+++ gasolina.py
@@ -0,0 +1,38 @@
+import csv
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#escrevendo o arquivo csv
+fields = ['Dia', 'Venda']
+
+rows = [ ['1', '5.11'],
+         ['2', '4.99'],
+         ['3', '5.02'],
+         ['4', '5.21'],
+         ['5', '5.07'],
+         ['6', '5.09'],
+         ['7', '5.13'],
+         ['8', '5.12'],
+         ['9', '4.94'],
+         ['10', '5.03'] ]
+
+filename = 'gasolina.csv'
+
+with open(filename, 'w') as file:
+  writer = csv.writer(file)
+  writer.writerow(fields)
+  writer.writerows(rows)
+
+  #criando o dataframe
+df = pd.read_csv('gasolina.csv')
+print(df.head())
+print(df.info())
+
+# visuzalização
+plt.figure(figsize=(8,5))
+sns.set_theme(style="darkgrid")
+grap = sns.lineplot(data=df, x='Dia', y='Venda', color='g')
+plt.title('Preço da gasolina no período')
+plt.show()
+
